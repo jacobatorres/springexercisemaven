fede6e7b6385b26479df0d6891b2f14404c1f2df
Jacob Torres <jacobangelo_torres@yahoo.com>
2020-07-13 08:40:46 -0700

    Put credentials in application config

--- src/main/java/com/jtorres/springexercisecrmaven/security/MyUserDetailsService.java
+++ src/main/java/com/jtorres/springexercisecrmaven/security/MyUserDetailsService.java
@@ -2,6 +2,7 @@ package com.jtorres.springexercisecrmaven.security;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,12 +11,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+	
+	@Value("${username_valid}")
+	private String username_config;
+
+	@Value("${password_valid}")
+	private String password_config;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		// username is the variable provided by the app
-		return new User("usern", "userp", new ArrayList<>());
+		return new User(username_config, password_config, new ArrayList<>());
 	}
 
 }
